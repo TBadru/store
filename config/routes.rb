9af988634ba724676b383ae7a1d7a1cdc7fe166c
TBadru <toheebbadru97@yahoo.com>
2025-08-22 20:18:24 +0100

    Implement email confirmation feature and user management with admin access controls

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,20 @@ Rails.application.routes.draw do
   root "products#index"
 
   namespace :settings do
+  resource :email, only: [ :show, :update ]
   resource :password, only: [ :show, :update ]
   resource :profile, only: [ :show, :update ]
+  resource :user, only: [ :show, :destroy ]
 
   root to: redirect("/settings/profile")
+end
+
+namespace :email do
+  resources :confirmations, param: :token, only: [ :show ]
+end
+
+# Admins Only
+namespace :store do
+  resources :users
 end
 end
